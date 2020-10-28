0dc0b9785486a5d1a3ad6de707ead8b3bf9c1dab
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2020-10-28 13:08:19 +0000

    Sanitise slashes in path mock

--- src/__mocks__/path.ts
+++ src/__mocks__/path.ts
@@ -5,6 +5,7 @@ const _relative = _path.relative;
 const resolve = (path: string) =>
   _resolve(path)
     .replace(_resolve('./'), '/project')
+    .replace(/\\/g, '/')
     .replace(_resolve('/'), 'R://');
 
 const relative = (a: string, b: string) => _relative(resolve(a), resolve(b));
