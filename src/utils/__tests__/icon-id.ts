847bc7f290af54ca6c0d3e6009e0316d49714821
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2020-10-28 22:19:47 +0000

    Use slugify and ensure slugs maintain underscores

--- src/utils/__tests__/icon-id.ts
+++ src/utils/__tests__/icon-id.ts
@@ -5,6 +5,8 @@ describe('Icon ID utilities', () => {
     expect(getIconId('./foo/bar/icon.svg', 'foo/bar')).toBe('icon');
     expect(getIconId('foo/bar/icon.svg', './foo')).toBe('bar-icon');
     expect(getIconId('foo/icon.SVG', 'foo')).toBe('icon');
+    expect(getIconId('foo/icon-test_1_a', 'foo')).toBe('icon-test_1_a');
+    expect(getIconId('foo/ICON-test', 'foo')).toBe('ICON-test');
     expect(getIconId('foo/test/icon-test.foo.Sv g', 'foo')).toBe(
       'test-icon-test-foo'
     );
