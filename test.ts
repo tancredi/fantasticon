e9db44ae578ea5907ec93731fffd26b25c3ca51d
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2020-11-07 16:58:36 +0000

    Remove test file

--- test.ts
+++
@@ -1,6 +0,0 @@
-import { generateFonts } from './src';
-
-generateFonts({
-  inputDir: '/Users/tancreditrugenberger/Desktop/Fantasticon/test/icons',
-  outputDir: '/Users/tancreditrugenberger/Desktop/Fantasticon/test/dist'
-}).then(results => console.log(results.codepoints));
