0f9cce980ead39d1631d79b235c67d0ac31e4cc8
janbiasi <jan.biasi@namics.com>
2020-11-09 17:04:29 +0100

    feat(scss,sass): add sass and scss asset options [#12]

---
+++ src/generators/asset-types/__tests__/scss.ts
@@ -0,0 +1,75 @@
+import scssGen from '../scss';
+import { renderSrcAttribute } from '../../../utils/css';
+import { resolve } from 'path';
+
+const renderSrcMock = (renderSrcAttribute as any) as jest.Mock;
+
+const mockOptions = {
+  name: 'test-font',
+  prefix: 'tf',
+  tag: 'b',
+  codepoints: { 'my-icon': 0xf101 },
+  assets: { 'my-icon': null },
+  templates: {
+    scss: resolve(__dirname, '../../../../templates/scss.hbs')
+  }
+} as any;
+
+jest.mock('../../../utils/css', () => ({
+  renderSrcAttribute: jest.fn(() => '"::src-attr::"')
+}));
+
+describe('`SCSS` asset generator', () => {
+  beforeEach(() => {
+    renderSrcMock.mockClear();
+  });
+
+  test('renders SCSS correctly with prefix and tag name options', async () => {
+    expect(
+      await scssGen.generate(mockOptions, Buffer.from(''))
+    ).toMatchSnapshot();
+  });
+
+  test('renders SCSS correctly with `selector` option', async () => {
+    expect(
+      await scssGen.generate(
+        { ...mockOptions, selector: '.my-selector' },
+        Buffer.from('')
+      )
+    ).toMatchSnapshot();
+  });
+
+  test('calls renderSrcAttribute correctly and includes its return value in the rendered template', async () => {
+    const fontBuffer = Buffer.from('::svg-content::');
+
+    const result = await scssGen.generate(mockOptions, fontBuffer);
+
+    expect(renderSrcMock).toHaveBeenCalledTimes(1);
+    expect(renderSrcMock).toHaveBeenCalledWith(mockOptions, fontBuffer);
+    expect(result).toContain('::src-attr::');
+  });
+
+  test('renders expected selector blocks', async () => {
+    const scss = await scssGen.generate(mockOptions, Buffer.from(''));
+
+    expect(scss).toContain('b[class^="tf-"]:before, b[class*=" tf-"]:before {');
+    expect(scss).toContain('.tf-my-icon:before {');
+  });
+
+  test('renders expected variables', async () => {
+    const scss = await scssGen.generate(mockOptions, Buffer.from(''));
+
+    expect(scss).toContain('$tf-test-font-font-family:');
+    expect(scss).toContain('$tf-my-icon:');
+  });
+
+  test('renders expected selector blocks with `selector` option', async () => {
+    const scss = await scssGen.generate(
+      { ...mockOptions, selector: '.my-selector' },
+      Buffer.from('')
+    );
+
+    expect(scss).toContain('.my-selector:before {');
+    expect(scss).toContain('.my-selector.tf-my-icon:before {');
+  });
+});
---
+++ src/generators/asset-types/scss.ts
@@ -0,0 +1,17 @@
+import { FontGenerator } from '../../types/generator';
+import { FontAssetType } from '../../types/misc';
+import { renderTemplate } from '../../utils/template';
+import { renderSrcAttribute } from '../../utils/css';
+
+const generator: FontGenerator<Buffer> = {
+  dependsOn: FontAssetType.SVG,
+
+  generate: (options, svg: Buffer) =>
+    renderTemplate(
+      options.templates.scss,
+      { ...options, fontSrc: renderSrcAttribute(options, svg) },
+      { helpers: { codepoint: str => str.toString(16) } }
+    )
+};
+
+export default generator;
