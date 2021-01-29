d814a674c585b2331c7c661076e485f1b4dabbb1
nfxpnk <nfxpnk@gmail.com>
2021-01-29 12:58:56 +0200

    To be able to sort icons

--- src/utils/icon-id.ts
+++ src/utils/icon-id.ts
@@ -2,11 +2,11 @@ import { resolve, relative } from 'path';
 import slug from 'slugify';
 import { removeExtension } from './path';
 
-export const getIconId = (filepath: string, root: string) =>
-  slug(
-    removeExtension(relative(resolve(root), resolve(filepath))).replace(
-      /(\/|\\|\.)+/g,
-      '-'
-    ).substr(4),
-    { replacement: '-' }
-  );
+export const getIconId = (filepath: string, root: string) => {
+  let iconId = removeExtension(relative(resolve(root), resolve(filepath))).replace(/(\/|\\|\.)+/g, '-');
+  let matches = iconId.match(/^\d{3}\-/);
+  if(matches) {
+    iconId = iconId.substr(4);
+  }
+  return slug(iconId, { replacement: '-' });
+}
